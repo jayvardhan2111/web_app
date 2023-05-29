27c8eb24ad6e2921f8beaacf4e7ea16aafcf3bf0
jayvardhan <jayvardhanprajapat@gmail.com>
2023-05-29 11:42:44 +0530

    added code

---
+++ web_app.go
@@ -0,0 +1,14 @@
+package main
+import (
+ "fmt"
+ "net/http"
+)
+func main() {
+ http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+ fmt.Fprint(w, "Hello!")
+ })
+ err := http.ListenAndServe("0.0.0.0:80", nil)
+ if err != nil {
+ panic(err)
+ }
+}
