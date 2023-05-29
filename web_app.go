d89673e8accbbb18ee63c963f069057cc6df67e2
Jayvardhan Prajapat <103930724+jayvardhan2111@users.noreply.github.com>
2023-05-29 12:24:15 +0530

    Update web_app.go

--- web_app.go
+++ web_app.go
@@ -5,7 +5,7 @@ import (
 )
 func main() {
  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
- fmt.Fprint(w, "Hello!")
+ fmt.Fprint(w, "Hello Wolrd!")
  })
  err := http.ListenAndServe("0.0.0.0:80", nil)
  if err != nil {
